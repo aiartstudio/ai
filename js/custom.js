1a9517a2686890cb09a07a522052dfe699125ddd
aiartstudio <83060154+aiartstudio@users.noreply.github.com>
2021-08-27 14:55:10 +0530

    b

--- js/custom.js
+++ js/custom.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 
     var userFeed = new Instafeed({
         get: 'user',
-        userId: 'YOURUSERID',
-        limit: 12,
+        userId: '47163674962',
+        limit: 5,
         resolution: 'standard_resolution',
-        accessToken: 'YOUR ACCESS ID',
+        accessToken: 'IGQVJVLXEwNXFSTDJLM1FJbUo5SFZAUSjNXYWFqUExBd24yNngxM3dEN0o3bGhzUkJ5aldFYjAxYV9sRzV0NUU0ZA0VMUUFkU1BXYkYtQXhuQW1abjJBbjhIUEtyRVg3MExCa0NJNEExSkZAUaFBXb3hEcwZDZD',
         sortBy: 'most-recent',
         template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
     });
